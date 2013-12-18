0e1378151b0ee3ff8d960ad7504f3cfaed903419
Albert_McKeever <kotn_ep1@hotmail.com>
2013-12-18 16:26:13 -0500

    modified readme

--- lib/movieDB/base.rb
+++ lib/movieDB/base.rb
@@ -3,7 +3,7 @@ require 'MovieDB/status_checker'
 require 'MovieDB/movie_error'
 
 module MovieDB #:nodoc
-# MoviesDB v0.1.x is not a datastore gem. Rather, it is a high-level statistical sftware that performs
+# MoviesDB v0.1.x is not a datastore gem. Rather, it is a high-level statistical software that performs
 # mathematical computations for analyzing film data from imdb.  
 # In a nut shell, it is a solution to the common problem of deducing logical hypothesis based off data sets.
   
