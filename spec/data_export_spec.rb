ad0f9e499c9a3ecad95ec3221a8b5a55e83ad490
keeperofthenecklace <kotn_ep1@hotmail.com>
2015-06-12 06:15:58 -0400

    fixed rspec test

--- spec/data_export_spec.rb
+++ spec/data_export_spec.rb
@@ -1,15 +1,13 @@
 require 'spec_helper'
 
 describe MovieDB::DataExport do
-  
-  describe "#export_movie_data" do
-      MovieDB::Movie.send(:clear_data_store)
-      MovieDB::Movie.send(:get_multiple_imdb_movie_data, "2024544", "1800241", "0791314")
-      MovieDB::DataExport.export_movie_data
 
-      let(:file_name) {MovieDB::DataExport.write_xls_file}
+  describe "#fetch_movie_data_from_imdb" do
+      MovieDB::Movie.get_data("2024544", "1800241", "0791314")
 
-    it 'writes the exported data as an xls file to the reports directory' do
+      let(:file_name) {MovieDB::Movie.write_imdb_data_to_xls}
+
+    it 'writes the fetched data as an xls file to the reports directory' do
      File.exist?(File.join('reports', file_name)).should == true
     end
 
