310cf18ae9032a4024e2e7e6ade81e0ea35dc843
keeperofthenecklace <kotn_ep1@hotmail.com>
2015-07-13 21:21:20 -0400

    Setup rspec test suite

---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+$TESTING = true
+
+require "simplecov"
+require "coveralls"
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start do
+  add_filter "/spec/"
+  minimum_coverage(92.21)
+end
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
+require "rspec"
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
